5c0bd24c6474dff2bfb437d79303e68134a4e3f1
rosalierazonable <rsrazonable1@up.edu.ph>
2021-05-06 01:35:52 +0800

    added proper documentation

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
 /*
  *  CMSC170: INTRODUCTION TO ARTIFICIAL INTELLIGENCE
+ *  RAZONABLE, ROSALIE S.
+ *
  *  AI SEARCH PROBLEM - 8 PUZZLE
  *  USING A* ALGORITHM
- *  - Rosalie Razonable
+ *
  */
 
 #include <iostream>
 #include "State.h"
+
 using namespace std;
 
 bool isGoalState(State& s, State& goalState);
 
 int main() {
-    State goalState(3, {0, 1, 2, 3, 4, 5, 6, 7, 8});
+    State goalState(3, {0, 1, 2, 3, 4, 5, 6, 7, 8}); //default goal state
     State s;
 //    State s(3, {0, 1, 2, 3, 4, 5, 6, 7, 8});
 
-    s.displayStateConfig();
-    s.createBoard();
-    cout << s.locateEmptyTile() << endl;
+//    s.displayStateConfig();
+//    s.createBoard();
+//    cout << s.locateEmptyTile() << endl;
 //    s.swapTiles(7).createBoard();
-    cout << isGoalState(s, goalState);
+//    cout << isGoalState(s, goalState) << endl;
+
     return 0;
 }
 
+// Auxiliary Methods
 bool isGoalState(State& s, State& goalState) {
     return goalState.getState() == s.getState();
 }
