cfc64b23ae492700bc9b92cce8aa578d2890637e
rosalierazonable <rsrazonable1@up.edu.ph>
2021-05-13 23:01:19 +0800

    refactor main program

--- main.cpp
+++ main.cpp
@@ -8,44 +8,33 @@
  */
 
 #include <iostream>
-#include "State.h"
-#include "Neighbour.h"
 #include "Board.h"
+#include "Neighbour.h"
 #include "Node.h"
 
 using namespace std;
 
 bool isGoalState(State& s, State& goalState);
+
+// Declaration of static attributes
 map<unsigned int, vector<int>> Neighbour::neighbourMap = {};
 map<unsigned int, vector<int>> Board::BoardIdxMap = {};
 
 int main() {
-//    Neighbour neighbour;
     State goalState(3, {0, 1, 2, 3, 4, 5, 6, 7, 8}); //default goal state
 
     State s;
 
     s.createBoard();
     cout << endl;
-    Node *root = new Node(s);
-    root->level = 0;
-    root->child = new Node(s.swapTiles(1));
-  /*  cout << "Hamming: " << root->calculateHammingCost(goalState) << endl;
-    cout  << "Manhattan: " << root->calculateManhattanCost(goalState) << endl;
-    root->setPathCostValue(goalState);
-    cout << "PathCost: " << root->getPathCostValue() << endl;
-*/
-    root->child->level = 1;
-    Node::printKthChild(root, s, 1);
-    root->child->sibling = new Node(s.swapTiles(3));
-    root->child->sibling->level = 1;
-    Node::printKthChild(root, s, 2);
-    root->child->sibling->sibling = new Node(s.swapTiles(5));
-    root->child->sibling->sibling->level = 1;
-    Node::printKthChild(root, s, 3);
-    root->child->sibling->sibling->sibling = new Node(s.swapTiles(7));
-    root->child->sibling->sibling->sibling->level = 1;
-    Node::printKthChild(root, s, 4);
+
+    Node *r = new Node(s);
+    r->setLevel(1);
+    r->setChild(new Node(s.swapTiles(1)));
+    r->getChild()->setLevel(1);
+    r->setPathCostValue(goalState);
+    cout << r->getPathCostValue();
+    Node::printKthChild(r, s, 1);
 
     return 0;
 }
